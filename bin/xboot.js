a26d5d14e5330fe7d1f0d11ffac986efb00f16f2
kiba.x.zhao <kiba.rain@qq.com>
2020-12-08 21:54:56 +0800

    初始化项目

---
+++ bin/xboot.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/**
+ * @fileOverview 命令行入口
+ * @name xboot.js
+ * @author kiba.x.zhao <kiba.rain@qq.com>
+ * @license MIT
+ */
+'use strict';
+
+const { Boot } = require('..');
+
+(new Boot()).init();
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @fileOverview 目录文件
+ * @name index.js
+ * @author kiba.x.zhao <kiba.rain@qq.com>
+ * @license MIT
+ */
+'use strict';
+
+const Boot = require('./lib/boot');
+
+exports.Boot = Boot;
