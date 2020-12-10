54ab23b5e56bdb5ee956c0e0623cee7b7e814636
kiba.x.zhao <kiba.rain@qq.com>
2020-12-10 21:51:39 +0800

    完成基本加载功能代码

--- bin/xboot.js
+++ bin/xboot.js
@@ -9,5 +9,23 @@
 'use strict';
 
 const { Boot } = require('..');
+const pkg = require('../package.json');
+const { program } = require('commander');
+program.version(pkg.version);
 
-(new Boot()).init();
+program
+  .option('-a, --all', 'load all match files',false)
+  .option('-d, --dir <type>', 'target directory',process.cwd())
+  .option('-r, --reverse', 'reverse load base files ',false)
+  .option('-p, --plugin', 'load plugin files',false);
+
+program.parse(process.argv);
+
+let patterns = [];
+if(program.args.length>0)
+  patterns.push(...program.args);
+else
+  patterns.push(`${pkg.name}.js`);
+
+const {all,...opts} = program.opts();
+(new Boot(patterns,opts)).init(()=>all);
