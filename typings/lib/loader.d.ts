ce4fbb848c08a2317097237426ba18dcc21e62f8
kiba.x.zhao <kiba.rain@qq.com>
2021-01-05 21:51:35 +0800

    Loader,ModuleLoader添加jsdoc注释

--- typings/lib/loader.d.ts
+++ typings/lib/loader.d.ts
@@ -1,5 +1,18 @@
 export = Loader;
+/**
+ * 加载器基类
+ * @class
+ */
 declare class Loader {
-    filter(fn: any): Generator<any, void, unknown>;
-    forEach(fn: any): void;
+    /**
+     * 过滤方法
+     * @param {Function} fn 过滤函数
+     * @yields {any} 匹配项
+     */
+    filter(fn: Function): Generator<any, void, unknown>;
+    /**
+     * 遍历加载项方法
+     * @param {Function} fn 加载项处理函数
+     */
+    forEach(fn: Function): void;
 }
--- typings/lib/module_loader.d.ts
+++ typings/lib/module_loader.d.ts
@@ -1,9 +1,53 @@
 export = ModuleLoader;
+/**
+ * 模块加载器类
+ * @class
+ */
 declare class ModuleLoader extends Loader {
-    constructor(patterns: any, modulesOpts: any, opts: any);
+    /**
+     * 模块可选项
+     * @typedef {Object} ModuleOpts
+     * @property {String} cwd 模块加载执行目录
+     */
+    /**
+     * 模块加载器可选项
+     * @typedef {Object} ModuleLoaderOpts
+     * @property {Boolean} reverse 是否逆序加载
+     * @property {Array<String> | String} mode 匹配模式
+     */
+    /**
+     * 构造方法
+     * @param {Array<String> | String} patterns 模块匹配模式
+     * @param {Array<ModuleOpts>} modulesOpts 模块可选项
+     * @param {ModuleLoaderOpts} opts 模块加载器可选项
+     */
+    constructor(patterns: Array<string> | string, modulesOpts: {
+        /**
+         * 模块加载执行目录
+         */
+        cwd: string;
+    }[], opts: {
+        /**
+         * 是否逆序加载
+         */
+        reverse: boolean;
+        /**
+         * 匹配模式
+         */
+        mode: Array<string> | string;
+    });
+    /**
+     * 类实例迭代器方法
+     * @yields {any} 加载模块项
+     */
     [Symbol.iterator](): Generator<any, void, unknown>;
-    [PROPERTY_PATTERNS]: any;
-    [PROPERTY_MODULES_OPTS]: any[];
+    [PROPERTY_PATTERNS]: string | string[];
+    [PROPERTY_MODULES_OPTS]: {
+        /**
+         * 模块加载执行目录
+         */
+        cwd: string;
+    }[];
 }
 import Loader = require("./loader");
 declare const PROPERTY_PATTERNS: unique symbol;
