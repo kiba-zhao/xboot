9fa8333e6a6fc449fe18bcc833d64fdfd1b7599e
kiba.x.zhao <kiba.rain@qq.com>
2021-01-06 20:32:42 +0800

    补充BootLoader,PluginModuleLoader的jsdoc说明

--- typings/lib/boot_loader.d.ts
+++ typings/lib/boot_loader.d.ts
@@ -1,12 +1,103 @@
 export = BootLoader;
+/**
+ * 引导加载器
+ * @class
+ */
 declare class BootLoader extends Loader {
-    constructor(patterns: any, opts?: {});
+    /**
+     * 模块可选项
+     * @typedef {Object} BootLoaderOpts
+     * @property {Array<String> | String} config 配置文件匹配模式
+     * @property {String} chdir 引导运行目录
+     * @property {Boolean} reverse 是否逆序加载
+     * @property {Array<String> | String} plugin 插件设置文件匹配模式
+     * @property {String} mode 引导过滤模式
+     */
+    /**
+     * 构造方法
+     * @param {Array<String> | String} patterns 引导文件匹配模式
+     * @param {BootLoaderOpts} opts 引导加载器可选项
+     */
+    constructor(patterns: Array<string> | string, opts?: {
+        /**
+         * 配置文件匹配模式
+         */
+        config: Array<string> | string;
+        /**
+         * 引导运行目录
+         */
+        chdir: string;
+        /**
+         * 是否逆序加载
+         */
+        reverse: boolean;
+        /**
+         * 插件设置文件匹配模式
+         */
+        plugin: Array<string> | string;
+        /**
+         * 引导过滤模式
+         */
+        mode: string;
+    });
+    /**
+     * 类实例迭代器方法
+     * @yields {any} 加载模块项
+     */
     [Symbol.iterator](): Generator<any, void, undefined>;
-    [PROPERTY_MODULE_LOADER]: ModuleLoader;
-    [PROPERTY_PLUGIN_MODULE_LOADER]: PluginModuleLoader;
+}
+declare namespace BootLoader {
+    export { ProfileOpts, ChildProfileOpts, Profile };
 }
 import Loader = require("./loader");
-declare const PROPERTY_MODULE_LOADER: unique symbol;
-import ModuleLoader = require("./module_loader");
-declare const PROPERTY_PLUGIN_MODULE_LOADER: unique symbol;
-import PluginModuleLoader = require("./plugin_module_loader");
+/**
+ * 引导配置文件可选项
+ */
+type ProfileOpts = {
+    /**
+     * 引导包名或包目录路径
+     */
+    engine: string;
+    /**
+     * 插件设置文件匹配模式
+     */
+    plugin: Array<string> | string;
+    /**
+     * 引导模式
+     */
+    modes: Array<string>;
+};
+/**
+ * 子引导配置文件可选项
+ */
+type ChildProfileOpts = {
+    /**
+     * 子引导包目录
+     */
+    cwd: string;
+};
+/**
+ * 引导配置
+ */
+type Profile = {
+    /**
+     * 引导包名或包目录路径
+     */
+    engine: string;
+    /**
+     * 包目录路径
+     */
+    cwd: string;
+    /**
+     * 插件设置文件匹配模式
+     */
+    plugin: Array<string> | string;
+    /**
+     * 引导模式
+     */
+    modes: Array<string>;
+    /**
+     * 当前配置匹配模式
+     */
+    patterns: Array<string> | string;
+};
--- typings/lib/plugin_module_loader.d.ts
+++ typings/lib/plugin_module_loader.d.ts
@@ -1,9 +1,133 @@
 export = PluginModuleLoader;
+/**
+ * 插件模块加载器
+ * @class
+ */
 declare class PluginModuleLoader extends Loader {
-    constructor(patterns: any, modulesOpts: any, opts?: {});
+    /**
+     * 模块可选项
+     * @typedef {Object} ModuleOpts
+     * @property {String} cwd 模块加载执行目录
+     * @property {Array<String> | String} plugin 插件匹配模式
+     * @property {Array<String> | String} pluginConfig 插件配置匹配模式
+     */
+    /**
+     * 模块加载器可选项
+     * @typedef {Object} PluginModuleLoaderOpts
+     * @property {Array<String> | String} mode 匹配模式
+     */
+    /**
+     * 构造方法
+     * @param {Array<String> | String} patterns 插件模块匹配模式
+     * @param {Array<ModuleOpts>} modulesOpts 模块可选项
+     * @param {PluginModuleLoaderOpts} opts 插件模块加载器可选项
+     */
+    constructor(patterns: Array<string> | string, modulesOpts: {
+        /**
+         * 模块加载执行目录
+         */
+        cwd: string;
+        /**
+         * 插件匹配模式
+         */
+        plugin: Array<string> | string;
+        /**
+         * 插件配置匹配模式
+         */
+        pluginConfig: Array<string> | string;
+    }[], opts?: {
+        /**
+         * 匹配模式
+         */
+        mode: Array<string> | string;
+    });
+    /**
+     * 类实例迭代器方法
+     * @yields {any} 加载模块项
+     */
     [Symbol.iterator](): Generator<any, void, undefined>;
-    [PROPERTY_MODULE_LOADER]: any[] | ModuleLoader;
+}
+declare namespace PluginModuleLoader {
+    export { PluginConfig, PluginSettings };
 }
 import Loader = require("./loader");
-declare const PROPERTY_MODULE_LOADER: unique symbol;
-import ModuleLoader = require("./module_loader");
+/**
+ * 插件配置
+ */
+type PluginConfig = {
+    /**
+     * 模块加载可选项
+     */
+    opts: {
+        /**
+         * 模块加载执行目录
+         */
+        cwd: string;
+        /**
+         * 插件匹配模式
+         */
+        plugin: Array<string> | string;
+        /**
+         * 插件配置匹配模式
+         */
+        pluginConfig: Array<string> | string;
+    };
+    /**
+     * 插件配置文件匹配模式
+     */
+    config: string;
+    /**
+     * 插件包名/包路径
+     */
+    package: string;
+};
+/**
+ * 插件设置
+ */
+type PluginSettings = {
+    /**
+     * 模块加载可选项
+     */
+    opts: {
+        /**
+         * 模块加载执行目录
+         */
+        cwd: string;
+        /**
+         * 插件匹配模式
+         */
+        plugin: Array<string> | string;
+        /**
+         * 插件配置匹配模式
+         */
+        pluginConfig: Array<string> | string;
+    };
+    /**
+     * 插件配置文件匹配模式
+     */
+    config: string;
+    /**
+     * 插件包名/插件包路径
+     */
+    package: string;
+    /**
+     * 插件包目录
+     */
+    cwd: string;
+    /**
+     * 插件包名称
+     */
+    name: string;
+    /**
+     * 插件模式
+     */
+    modes: Array<string>;
+    /**
+     * 依赖项
+     */
+    dependencies: Array<string>;
+    /**
+     * 可选依赖项
+     */
+    optionalDependencies: Array<string>;
+};
